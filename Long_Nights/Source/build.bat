a63b9bf7d1886a2ffc0278a22f346e9fe420f6ce
Radu-Dumitru Chira <chiraradu2004@gmail.com>
2022-12-10 02:07:07 +0100

    Rendering smth

--- Long_Nights/Source/build.bat
+++ Long_Nights/Source/build.bat
@@ -1,4 +1,4 @@
 @echo off
-pushd ..\..\..\build
-cl -Zi ..\Long_Nights\Source\main.cpp User32.lib
+pushd ..\..\build
+cl -Zi ..\Long_Nights\Source\main.cpp User32.lib Gdi32.lib
 popd
--- Long_Nights/Source/main.cpp
+++ Long_Nights/Source/main.cpp
@@ -1,11 +1,116 @@
 #include <Windows.h>
+#include <stdint.h>
 
-static bool s_isRunning;
+#define internal static
+#define local_presist static
+#define global_variable static
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+global_variable bool s_isRunning;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+int BytesPerPixel = 4;
+
+internal void
+RenderWeirdGradient(int XOffset, int YOffset)
 {
+    int Width = BitmapWidth;
+    int Height = BitmapHeight;
+    int Pitch = Width*BytesPerPixel;
+    uint8 *Row = (uint8 *)BitmapMemory;
+    for (size_t i = 0; i < BitmapHeight; i++)
+    {
+        uint8 *Pixel = (uint8 *)Row;
+        for (size_t j = 0; j < BitmapWidth; j++)
+        {
+            *Pixel = (uint8)i;
+            ++Pixel;
+
+            *Pixel = (uint8)j;
+            ++Pixel;
+
+            *Pixel = 0;
+            ++Pixel;
+
+            *Pixel = 0;
+            ++Pixel;
+        }
+        Row += Pitch;
+    }
+}
+
+
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+
+    if (BitmapMemory)
+    {
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE); 
+    }
+
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+    BitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biSizeImage = 0;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    int BitmapMemorySize = (BitmapWidth * BitmapHeight)*BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+    
+    RenderWeirdGradient(0, 0);
+    
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height)
+{
+
+    int WindowWidth = WindowRect->right - WindowRect->left;
+    int WindowHeight = WindowRect->bottom - WindowRect->top;
+
+    StretchDIBits(DeviceContext, 
+    0, 0, BitmapWidth, BitmapHeight,
+    0, 0, WindowWidth, WindowHeight,
+    BitmapMemory, 
+    &BitmapInfo, 
+    DIB_RGB_COLORS, SRCCOPY);
+}
+
+
+LRESULT CALLBACK 
+WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    
     switch (message)
     {
+    case WM_SIZE:
+    {
+        RECT ClientRect;
+        GetClientRect(hWnd, &ClientRect);
+        int Width = ClientRect.right - ClientRect.left;
+        int Height = ClientRect.bottom - ClientRect.top;
+        Win32ResizeDIBSection(Width, Height);
+        OutputDebugStringA("Window sized");
+
+        return (0);
+    } break;
     case WM_CREATE:
     {
         OutputDebugStringA("Window created");
@@ -21,18 +126,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         return (0);
     } break;
     
-    case WM_SIZE:
-    {
-        OutputDebugStringA("Window sized");
-
-        return (0);
-    } break;
 
     case WM_QUIT:
     {
         s_isRunning = false;
         PostQuitMessage(0);
     } break;
+    
+    case WM_PAINT:
+    {
+        RECT ClientRect;
+        GetClientRect(hWnd, &ClientRect);
+        PAINTSTRUCT ps;
+        HDC handleDevice = BeginPaint(hWnd, &ps);
+        int X = ps.rcPaint.left;
+        int Y = ps.rcPaint.top;
+        int Width = ps.rcPaint.right - ps.rcPaint.left;
+        int Height = ps.rcPaint.bottom - ps.rcPaint.top;
+        Win32UpdateWindow(handleDevice, &ClientRect, X, Y, Width, Height);
+        //PatBlt(handleDevice, X, Y, Width, Height, BLACKNESS);
+        EndPaint(hWnd, &ps);
+    }
     
     default:
         break;
@@ -57,14 +171,14 @@ WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdSh
     WindowClass.hbrBackground = nullptr;
     WindowClass.cbWndExtra = 0;
     WindowClass.lpszClassName = "Hello";
-    WindowClass.lpszMenuName = 0;
+    WindowClass.lpszMenuName = nullptr;
 
     if (RegisterClassEx(&WindowClass))
     {
         HWND WindowHandle = CreateWindowEx(0, 
         WindowClass.lpszClassName, "MainWindow", 
         WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
-        200, 200, 640, 480, 0, 0, hInstance, 0);
+        200, 200, 1280, 720, 0, 0, hInstance, 0);
 
         if (WindowHandle)
         {
@@ -73,31 +187,30 @@ WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdSh
             while (s_isRunning)
             {
                 MSG message;
-                BOOL messageResult = PeekMessage(&message, WindowHandle, 0, 0, PM_REMOVE);
+                
 
-                if (messageResult > 0)
+                while (PeekMessage(&message, WindowHandle, 0, 0, PM_REMOVE))
                 {
+                    if (message.message == WM_QUIT)
+                    {
+                        s_isRunning = false;
+                    }
+                    
                     TranslateMessage(&message);
                     DispatchMessage(&message);
                 }
-                
-                
-            }
-            
+            }   
         }
         else
         {
             OutputDebugStringA("Failed Window Handle");
             return (EXIT_FAILURE);
         }
-        
     }
     else
     {
         OutputDebugStringA("Failed register class");
         return (EXIT_FAILURE);
     }
-    
-
     return (EXIT_SUCCESS);
 }
